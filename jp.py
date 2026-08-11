db397d55bc89f8a75f72f626b12a02babb6faf5d
Jpoblete <japoblete_17@gmail.com>
2026-08-11 11:56:32 -0400

    archivo.py_punto_4_6

--- jp.py
+++ jp.py
@@ -98,7 +98,7 @@ if archivo is not None:
         .str.replace("-", "", regex=False)
         .str.replace(" ", "", regex=False)
     .str.strip()       # limpieza numeros de telefono _punto 4 JP-
-
+    )
     df["teléfono responsable"] = df["teléfono responsable"].str.slice(-9)
     df["teléfono responsable"] = df["teléfono responsable"].str.zfill(9)
     df["teléfono responsable"] = df["teléfono responsable"].apply(
